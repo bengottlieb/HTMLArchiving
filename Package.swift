07d7b4087772d56131c05655b4782798e8ebe87a
Ben Gottlieb <ben@standalone.com>
2020-11-25 07:59:55 -0600

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "HTMLArchiving",
+    platforms: [
+             .macOS(.v10_13),
+             .iOS(.v12),
+             .watchOS(.v5)
+        ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
@@ -12,15 +17,17 @@ let package = Package(
 			targets: ["HTMLArchiving", "ParseHTML"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/bengottlieb/Suite.git", from: "0.9.87"),
+//        .package(url: "https://bengottlieb@github.com/standalone/gulliver", from: "1.0.5"),
+        .package(url: "https://bengottlieb@github.com/bengottlieb/plug", from: "1.0.1"),
+        .package(url: "https://bengottlieb@github.com/bengottlieb/crossplatformkit", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "HTMLArchiving",
-			dependencies: ["https://bengottlieb@github.com/standalone/gulliver.git", "https://github.com/bengottlieb/plug.git", "https://github.com/bengottlieb/crossplatformkit.git"]),
+			dependencies: ["ParseHTML", "Plug", "CrossPlatformKit", "Studio"]),
 		.target(
 			name: "ParseHTML",
 			dependencies: []),
